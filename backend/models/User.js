0c77c301dcaabab9986c31c510b3fac6e0d9dc87
Rimpi <rimpiranibaruah123@gmail.com>
2021-07-16 12:14:45 +0530

    Register

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,45 +1,49 @@
-const mongoose = require("mongoose")
-const bcrypt = require("bcrypt")
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = mongoose.Schema(
-	{
-		name: {
-			type: String,
-			required: true,
-		},
-		email: {
-			type: String,
-			required: true,
-			unique: true,
-		},
-		password: {
-			type: String,
-			required: true,
-		},
-		isAdmin: {
-			type: Boolean,
-			default: false,
-		},
-		gender: {
-			type: String,
-			required: true,
-		},
-	},
-	{
-		timestamps: true,
-	}
-)
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+    gender: {
+      type: String,
+      required: true,
+    },
+    ethnicity: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 userSchema.methods.matchPassword = async function (enteredPassword) {
-	return await bcrypt.compare(enteredPassword, this.password)
-}
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
 userSchema.pre("save", async function (next) {
-	if (!this.isModified("password")) {
-		next()
-	}
-	const salt = await bcrypt.genSalt(10)
-	this.password = await bcrypt.hash(this.password, salt)
-})
-const User = mongoose.model("User", userSchema)
-module.exports = User
+  if (!this.isModified("password")) {
+    next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+const User = mongoose.model("User", userSchema);
+module.exports = User;
